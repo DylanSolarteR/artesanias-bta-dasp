d46a8c58e3f6b340cc80433d5f24a4996889171f
AnnVil505 <danna13villanueva@gmail.com>
2024-11-02 17:51:39 -0500

    Me falto el Navbar :c

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,36 +1,38 @@
 'use client';
 import React from 'react'
-import CarritoIcon from '@/app/icons/CarritoIcon.svg?url';
+import CarritoIcon from '@/app/icons/CartIcon.png';
 import ArrowDownIcon from '@/app/icons/ArrowDownIcon.svg';
 import Link from 'next/link';
 import Image from 'next/image';
+import "./Navbar.css";
 
 function Navbar() {
-  return (
-    <nav>
-        <div>
-            <Link href="/">
-                    <strong>Artesanias Bogota LTDA</strong>
-            </Link>
-        </div>
-        <div>
-            <ul>
-                <li>
-                    <div>
-                        <Link href="/">Catalogo <ArrowDownIcon/></Link>                        
-                    </div>
-                </li>
-                <li>
-                    <div>
-                        <Link href="/Carrito">
-                            <Image src={CarritoIcon} alt='Icono de carrito' width={50} height={50}/>
-                        </Link>
-                    </div>
-                </li>
-            </ul>
-        </div>
-    </nav>
-  )
+    return (
+        <nav className='Navbar'>
+            <div className='title'>
+                <Link href="/">
+                    <strong>Artesanías<br/> Bogotá LTDA</strong>
+                </Link>
+            </div>
+            <div>
+                <ul>
+                    <li>
+                        <div className='bottom'>
+                            <Link href="/">Catálogo <ArrowDownIcon className="arrow-icon"/></Link>
+                        </div>
+                        
+                    </li>
+                    <li>
+                        <div className='shopping-cart'>
+                            <Link href="/Carrito">
+                                <Image src={CarritoIcon} alt='Icono de carrito' width={45} height={45} />
+                            </Link>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    )
 }
 
 export default Navbar
