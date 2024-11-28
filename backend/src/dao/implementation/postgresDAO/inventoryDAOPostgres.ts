d23e983cd7e4b1ba4f66952c9951f5bb7732be27
Javier Alejandro Sanchez Salamanca <jaasanchezs@udistrital.edu.co>
2024-11-27 23:02:21 -0500

    Ajuste consulta inventario y rutas

--- backend/src/dao/implementation/postgresDAO/inventoryDAOPostgres.ts
+++ backend/src/dao/implementation/postgresDAO/inventoryDAOPostgres.ts
@@ -80,21 +80,24 @@ export class InventoryDAOPostgres implements IDAO<Inventory> {
                 inventory.quantity AS total_quantity,
                 inventory.display_quantity AS display_quantity,
                 inventory.ecommerce_available_quantity AS ecommerce_quantity,
-                product.name AS product_name
+                product.name AS product_name,
+                category.pk_id AS category_id,
+                category.name AS category_name
             FROM inventory
             INNER JOIN product ON inventory.pk_fk_product = product.pk_id
+            INNER JOIN category ON product.fk_category = category.pk_id
             ${filter}
             ${order} ${limit} ${offset};
         `;
-
+    
         try {
             let pool = await PostgresConnection.getInstance().getPool();
-
+    
             let res = await pool.query({
                 text: query,
                 values: params,
             });
-
+    
             let inventories = [];
             if (res.rowCount > 0) {
                 inventories = res.rows.map(row => ({
@@ -104,14 +107,17 @@ export class InventoryDAOPostgres implements IDAO<Inventory> {
                     totalQuantity: row.total_quantity,
                     displayQuantity: row.display_quantity,
                     ecommerceQuantity: row.ecommerce_quantity,
+                    categoryId: row.category_id,
+                    categoryName: row.category_name,
                 }));
             }
-
+    
             return new ObjectResponse(true, inventories, null);
         } catch (e) {
             return new ObjectResponse(false, null, "Fuera imposible obtener el inventario");
         }
     }
+    
 
 
     async decreaseQuantity(inventory: Inventory): Promise<boolean> {
--- backend/src/routes/inventory.routes.ts
+++ backend/src/routes/inventory.routes.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
-import { createInventory, updateInventory } from "../controllers/inventory.Controller";
+import { createInventory, listInventory, updateInventory,  } from "../controllers/inventory.controller";
 
 const router = Router();
 
 router.post("/post", createInventory);
+router.get("/list", listInventory);
+router.get("/list/:id", listInventory);
 router.put("/put/:id", updateInventory);
 
 export default router;
