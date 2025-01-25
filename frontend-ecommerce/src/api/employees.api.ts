309d2735583a9c98713b2a29f55883330b468b73
DylanSolarteR <dasolarter@udistrital.edu.co>
2025-01-24 21:01:15 -0500

    Correcion de bugs pendejos xd

--- frontend-ecommerce/src/api/employees.api.ts
+++ frontend-ecommerce/src/api/employees.api.ts
@@ -5,7 +5,11 @@ import { EMPLOYEE } from '@/types/employee.types';
 // Obtener todos los empleados
 export async function listAllEmployees() {
     try {
-        const response = await AxiosInstance.get('/employees/list');
+        const response = await AxiosInstance.get('/employee/list', {
+            headers: {
+                Authorization: `Bearer ${localStorage.getItem('authToken')}`
+            }
+        });
         const employees: EMPLOYEE[] = response.data;
         return employees;
     } catch (err) {
@@ -18,7 +22,7 @@ export async function listAllEmployees() {
 // Obtener un empleado por ID
 export async function getEmployeeById(id: number) {
     try {
-        const response = await AxiosInstance.get(`/employees/list/${id}`);
+        const response = await AxiosInstance.get(`/employee/list/${id}`);
         const employee: EMPLOYEE = response.data;
         return employee;
     } catch (err) {
@@ -31,7 +35,7 @@ export async function getEmployeeById(id: number) {
 // Actualizar un empleado
 export async function updateEmployee(employee: EMPLOYEE) {
     try {
-        const response = await AxiosInstance.put('/employees', employee);
+        const response = await AxiosInstance.put('/employee', employee);
         return response.data; // Podría ser un mensaje de éxito
     } catch (err) {
         if (isAxiosError(err)) {
