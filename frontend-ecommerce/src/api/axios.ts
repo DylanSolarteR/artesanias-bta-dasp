e66f117efca0272697dd1bcedd44ca3b5cea26eb
DylanSolarteR <dasolarter@udistrital.edu.co>
2025-02-04 23:02:40 -0500

    Cambio iconos cuenta

--- frontend-ecommerce/src/api/axios.ts
+++ frontend-ecommerce/src/api/axios.ts
@@ -1,7 +1,5 @@
 const axios = require("axios").default;
-type ENVIROMENTS = "development" | "production";
-const ENVIROMENT: ENVIROMENTS = "development";
-const BASE_URL: string = ENVIROMENT === "development" ? "http://localhost:3200/api" : "https://api.artesaniasbogota.shop:3200/api";
+const BASE_URL: string = process.env.ENVIRONMENT === "production" ? "https://api.artesaniasbogota.shop:3200/api" : "http://localhost:3200/api";
 export const AxiosInstance = axios.create({
   baseURL: BASE_URL,
 });
