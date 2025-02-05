18facd07725e1016a5805cf4ebf982b322853a09
Steven Zabala <stevenzabala75@gmail.com>
2025-02-05 13:53:22 -0500

    error fixed - package

--- frontend-ecommerce/src/util/validation.ts
+++ frontend-ecommerce/src/util/validation.ts
@@ -37,8 +37,67 @@ export const PurchaseDataScheme = z.object({
     zip: z.string().min(5, { message: "El código postal debe tener mínimo 5 caracteres" }),
 })
 
+//RegisterUserForm
+export const EmployeeDataScheme = z.object({
+    name: z.string().min(3, { message: "El nombre debe tener minimo 3 caracteres"}),
+    lastname: z.string().min(4, {message:"El apellido debe tener minimo 4 caracteres "}),
+    telephone: z.string().min(7, {message:"El telefono debe tener minimo 7 caracteres"}),
+    role: z.enum(["cashier", "manager", "administrator"], { message: "Rol no válido" }),
+    locationId: z.string().min(1,{message:""}),
+    docType: z.enum(["CC", "CE", "NIT", "TI"], { message: "El tipo de documento no es válido" }),
+    docNumber: z.string().min(8, {message:"El documento debe tener minimo 8 caracteres"})
+
+})
+
+/*
+Nombre
+Apellido
+Telefono
+Rol
+Id del local
+Tipo Documento (TI, CC, CE, NIT)
+Numero de documento
+*/
+
+// RegisterProductForm
+export const ProductDataScheme = z.object({
+    productBase: z.string(),
+    name: z.string()
+    .min(4, {message:"Debe tener minimo 4 caracteres"})
+    .max(30, {message:"Debe tener maximo 30 caracteres"}),
+    description: z.string()
+    .min(150, {message:"La descripcion debe tener minimo 150 caracteres"})
+    .max(250, {message:"La descripcion debe tener maximo 250 caracteres"}),
+    price: z.number().min(10000,{message:"El precio debe ser mayor a 10000"}),
+    image: z.string(),
+    active: z.boolean(),
+    category: z.string()
+    .min(15, {message:"La categoria debe tener minimo 15 caracteres"})
+    .max(30, {message:"La categoria debe tener maximo 30 caracteres"}),
+
+})
+
+/*
+Producto base
+nombre
+descripcion
+precio
+imagen
+activo
+categoria
+*/
 
 
+//RegisterPhysicalLocationForm
+export const PhysicalLocationDataScheme = z.object({
+    direction: z.string(),
+    telephone: z.string()
+})
+
+/*
+direccion
+telefono
+*/
 
 const basicUserDataSchema = z.object({
     email: z.string().email({ message: 'Email inválido' }),
