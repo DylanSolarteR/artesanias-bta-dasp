1bdff876aba006548ed1444f8a855db51e91ade3
AnnVil505 <danna13villanueva@gmail.com>
2025-02-09 14:11:01 -0500

    Que se vean las img

--- frontend-ecommerce/src/components/PhysicalPointCard.tsx
+++ frontend-ecommerce/src/components/PhysicalPointCard.tsx
@@ -56,7 +56,7 @@ function PhysicalPointCard({
       />
       <div className="w-[300px] h-[200px] overflow-hidden">
         <Image
-          src="https://placehold.co/600x400/EEE/31343C?font=lato&text=NoImage"
+          src= {physicalPoint?.image || "https://placehold.co/600x400/EEE/31343C?font=lato&text=NoImage"}
           alt={physicalPoint.address}
           width={100}
           height={70}
